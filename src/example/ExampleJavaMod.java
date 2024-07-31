6f20d748760e760173be98a662e87514b523e67c
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-07-31 17:36:13 +0200

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -18,14 +18,14 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                BaseDialog dialog = new BaseDialog("Neo-Dustry");
+                dialog.cont.add("Made by:").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("Okay", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
-        });
+        });d
     }
 
     @Override
