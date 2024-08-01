bb570185389464acb56f85183aa3cd551d0ddf0d
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-08-01 17:05:54 +0200

    Update NagyUnitTypes.java

--- src/nagyjavamod/NagyUnitTypes.java
+++ src/nagyjavamod/NagyUnitTypes.java
@@ -37,6 +37,7 @@ public class NagyUnitTypes {
             constructor = UnitEntity::create;
             hovering = true;
             shadowElevation = 0.1f;
+            lowAltitude = true;
             speed = 1.75f;
             accel = 0.2f;
             drag = 0.2f;
