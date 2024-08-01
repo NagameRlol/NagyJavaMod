8bac3cdd2ed48d91fbe45a0ab7a0077ad872fe20
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-08-01 13:57:40 +0200

    Update NagyUnitTypes.java

--- src/nagyjavamod/NagyUnitTypes.java
+++ src/nagyjavamod/NagyUnitTypes.java
@@ -1,5 +1,13 @@
 package nagyjavamod;
 
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
