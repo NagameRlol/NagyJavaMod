bf06792f6076830c473777d83ab0cccab926b1c7
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-08-01 16:35:32 +0200

    Update NagyUnitTypes.java

--- src/nagyjavamod/NagyUnitTypes.java
+++ src/nagyjavamod/NagyUnitTypes.java
@@ -59,7 +59,7 @@ public class NagyUnitTypes {
                 }},
                 new HoverPart(){{
                     x = 2f;
-                    y = -3f;
+                    y = 3f;
                     mirror = true;
                     radius = 5f;
                     phase = 90f;
