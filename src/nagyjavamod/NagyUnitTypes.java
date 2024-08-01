741cbbcf1af226e8d42f47d6491bb001ed05f79f
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-08-01 13:44:15 +0200

    Update NagyUnitTypes.java

--- src/nagyjavamod/NagyUnitTypes.java
+++ src/nagyjavamod/NagyUnitTypes.java
@@ -1,9 +1,26 @@
 package nagyjavamod;
 
 public class NagyUnitTypes {
-    public static UnitType shitass;
+    public static UnitType trooper;
 
     public static void load(){
-        shitass = new NagyUnitType("shitass");
+        trooper = new NagyUnitType("trooper"){{
+            speed = 0.7f;
+            hitSize = 10f;
+            health = 400;
+            weapons.add(new Weapon(""){{
+                reload = 5f;
+                x = 0f;
+                y = 5f;
+                top = false;
+                inacuraccy = 3
+                ejectEffect = Fx.casing1;
+                bullet = new BasicBulletType(5f, 10){{
+                    width = 9f;
+                    height = 12f;
+                    lifetime = 60f;
+                }};
+            }});
+        }};
     }
 }
