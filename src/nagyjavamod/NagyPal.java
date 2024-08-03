f8933e5893b76b964fe2940e83bf4f064895e46c
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2024-08-03 18:00:46 +0200

    Update NagyPal.java

--- src/nagyjavamod/NagyPal.java
+++ src/nagyjavamod/NagyPal.java
@@ -5,6 +5,6 @@ import arc.graphics.*;
 public class NagyPal {
   
     public static Color
-    basaltOutline = Color.valueOf("1e1e2e"),
+    basaltOutline = Color.valueOf("1d1d2b"),
     redLaser = Color.valueOf("ff7a70");
 }
