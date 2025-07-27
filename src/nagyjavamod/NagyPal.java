7295f3f04e1f30d778bbd8f6a7e55d01cdcea616
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2025-07-27 20:40:09 +0200

    Atualizar o NagyPal.java

--- src/nagyjavamod/NagyPal.java
+++ src/nagyjavamod/NagyPal.java
@@ -6,6 +6,6 @@ public class NagyPal {
   
     public static Color
     basaltOutline = Color.valueOf("1d1d2b"),
-    redLaser = Color.valueOf("ff7a70");
+    redLaser = Color.valueOf("ff7a70"),
     repairLaser = Color.valueOf("84f491");
 }
