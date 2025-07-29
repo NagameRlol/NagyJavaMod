3cb9a25e32d042aa225ebc4e6cd3965a47998a70
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2025-07-29 15:59:26 +0200

    fuckass mistake

--- src/nagyjavamod/NagyUnitTypes.java
+++ src/nagyjavamod/NagyUnitTypes.java
@@ -469,7 +469,7 @@ public class NagyUnitTypes {
             circleTarget = false;
 
             parts.add(new ShapePart(){{
-                        progress = PartProgress.smooothReload;
+                        progress = PartProgress.smoothReload;
                         color = NagyPal.newRedLaser;
                         sides = 4;
                         radius = 10f;
