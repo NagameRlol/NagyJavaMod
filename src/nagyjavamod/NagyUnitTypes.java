ae4ce8e18134d9855d3e21562c1d37df4e14ef25
NagyDoesStuff <91905724+NagameRlol@users.noreply.github.com>
2025-07-29 16:23:19 +0200

    asd

--- src/nagyjavamod/NagyUnitTypes.java
+++ src/nagyjavamod/NagyUnitTypes.java
@@ -476,7 +476,7 @@ public class NagyUnitTypes {
                 top = false;
                 inaccuracy = 0;
                 shootSound = Sounds.shootBig;
-                ejectEffect = Fx.spark;
+                ejectEffect = Fx.colorSpark;
                 bullet = new BasicBulletType(3f, 8f){{
                     width = 10f;
                     height = 10f;
